6f60d516dcf4f757c368c9ca595978bad5faaeda
George Orwell <george.orwel.1984@outlook.com>
2025-07-13 03:11:44 +0200

    Refactoring

--- modules/server/include/Server.h
+++ modules/server/include/Server.h
@@ -11,46 +11,37 @@ using PacketHandler = std::function<void(const std::error_code&, std::size_t, st
 /**
     A Server class that implements the GenericServer interface based on implmentation policy and a
    mode policy. The Backend Policy will decide whether this is a TCP or UDP based server and provide
-   a socket of that particular type. The ModePolicy will determine if the server should run in an
+   a socket of that particular type. The Mode will determine if the server should run in an
    asynchronous or synhcronous way.
  */
-template <typename BackendPolicy, template <typename> class ModePolicy>
+template <typename Protocol, template <typename> class Mode>
 class Server : public GenericServer
 {
 public:
-    using Socket = typename BackendPolicy::Socket;
-    using Endpoint = typename BackendPolicy::Endpoint;
-    using Context = typename BackendPolicy::Context;
-    using Error_Code = typename BackendPolicy::error_code;
+    using Socket = typename Protocol::Socket;
+    using Endpoint = typename Protocol::Endpoint;
+    using Context = typename Protocol::Context;
+    using Error_Code = typename Protocol::error_code;
 
-    //constructor
-    Server(Context& io, uint16_t port, PacketHandler handler = nullptr)
-        : m_socket(io), m_port(port), m_running(false)
-    {
-        m_buffer.resize(UDP_PACKET_SIZE);
-        m_packetHandler = (handler)
-                              ? std::move(handler)
-                              : std::bind(&Server::onPacketReceived, this, std::placeholders::_1,std::placeholders::_2, std::placeholders::_3);
-    }
+    // Constructor
+    Server(Context& io, uint16_t port, PacketHandler handler = nullptr);
 
-    // generic-server
+    // Generic-Server
     std::error_code start() override;
     std::error_code stop() override;
     std::error_code shutdown() override;
     
-    // packet-handling
-    using PacketHandler = std::function<void(const std::error_code &, std::size_t, std::string_view)>;    
+    // Packet Handling
     void setPacketHandler(PacketHandler handler) { m_packetHandler = std::move(handler); }
 
 protected:
-    // void receiveNextPacket();
     void onPacketReceived(const Error_Code& error, std::size_t len, std::string_view packet);
 
 private:
     Socket m_socket;
     Endpoint m_remoteEndpoint;    
     uint16_t m_port;
-    PacketHandler m_packetHandler;
     std::vector<char> m_buffer;
     std::atomic<bool> m_running;
+    PacketHandler m_packetHandler;
 };
--- modules/server/src/Server.cpp
+++ modules/server/src/Server.cpp
@@ -1,14 +1,25 @@
 #include "Server.h"
-#include "BoostUDPPolicy.hpp"
-#include "ModePolicy.hpp"
 
 #include <string>
 
-// Template method implementations
+#include "BoostUDPPolicy.hpp"
+#include "ModePolicy.hpp"
+
+// Constructor
+template <typename Protocol, template <typename> class Mode>
+Server<Protocol, Mode>::Server(Context& io, uint16_t port, PacketHandler handler)
+    : m_socket(io), m_port(port), m_running(false)
+{
+    m_buffer.resize(UDP_PACKET_SIZE);
+    m_packetHandler = (handler) ? std::move(handler)
+                                : std::bind(&Server::onPacketReceived, this, std::placeholders::_1,
+                                            std::placeholders::_2, std::placeholders::_3);
+}
+
 template <typename Protocol, template <typename> class Mode>
 std::error_code Server<Protocol, Mode>::start()
-{   
-    // Init socket
+{
+    // 1.   Init socket
     auto ec = Protocol::openAndBindSocket(m_socket, Protocol::makeEndpoint(m_port));
     if (ec)
     {
@@ -16,15 +27,16 @@ std::error_code Server<Protocol, Mode>::start()
         return ec;
     }
 
-    // Listen for packets
+    // 2.   Listen for packets
     m_running = true;
     Mode<Protocol>::receivePacket(m_running, m_socket, m_remoteEndpoint, m_buffer, m_packetHandler);
 
-    return { }; // success
+    return {};  // success
 }
 
 template <typename Protocol, template <typename> class Mode>
-void Server<Protocol, Mode>::onPacketReceived(const typename Protocol::error_code& error, std::size_t len, std::string_view packet)
+void Server<Protocol, Mode>::onPacketReceived(const typename Protocol::error_code& error,
+                                              std::size_t len, std::string_view packet)
 {
     Log("Received packet of size: " + std::to_string(len));
     Log("Packet data: " + std::string(packet));
@@ -34,14 +46,14 @@ template <typename Protocol, template <typename> class Mode>
 std::error_code Server<Protocol, Mode>::stop()
 {
     // Atomically set m_running to false and check if it was previously true
-    if (!m_running.exchange(false)) 
+    if (!m_running.exchange(false))
     {
         return {};
     }
 
-    typename Protocol::error_code error;        
+    typename Protocol::error_code error;
     m_socket.close(error);
-    if(error)
+    if (error)
     {
         Log(error);
         return error;
